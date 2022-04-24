2e4bc2545f6e1bad88dee0b6cb9ee6fbf912318c
Success John <successojohn@gmail.com>
2022-04-24 10:18:55 +0200

    Upload, Download and Deletion From Database

--- FileUpload/Controllers/FileUploadController.cs
+++ FileUpload/Controllers/FileUploadController.cs
@@ -103,7 +103,53 @@ namespace FileUpload.Controllers
             return RedirectToAction("Index");
         }
 
-       
+        public async Task<IActionResult> UploadToDatabase(List<IFormFile> files, string description)
+        {
+            foreach (var file in files)
+            {
+                var fileName = Path.GetFileNameWithoutExtension(file.FileName);
+                var extension = Path.GetExtension(file.FileName);
+                var fileModel = new FileOnDatabaseModel
+                {
+                    CreatedOn = DateTime.UtcNow,
+                    FileType = file.ContentType,
+                    Extension = extension,
+                    Name = fileName,
+                    Description = description
+                };
+
+                using (var dataStream = new MemoryStream())
+                {
+                    await file.CopyToAsync(dataStream);
+                    fileModel.Data = dataStream.ToArray();
+
+                }
+
+                _context.FilesOnDatabase.Add(fileModel);
+                _context.SaveChanges();
+            }
+
+            TempData["Message"] = "File Uploaded Successfully to Database";
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> DownloadFileFromDatabase(int id)
+        {
+            var file = await _context.FilesOnDatabase.Where(x => x.Id == id).FirstOrDefaultAsync();
+            if (file == null) return null;
+            return File(file.Data, file.FileType, file.Name + file.Extension);
+        }
+
+        public async Task<IActionResult> DeleteFileFromDatabase(int id)
+        {
+            var file = await _context.FilesOnDatabase.Where(x => x.Id == id).FirstOrDefaultAsync();
+            _context.FilesOnDatabase.Remove(file);
+            _context.SaveChanges();
+            TempData["Message"] = $"Removed {file.Name + file.Extension} successfully from Database.";
+            return RedirectToAction("Index");
+        }
+
+
     }
 
 
